9e461399d2cdec6895b89b1b842de99b6aca2a5d
Chris <celdridge91190@gmail.com>
2013-02-07 10:46:22 -0500

    Newer changes to BagCheck. This allows it to take the name of a passenger into account and send it back to the Line

--- TSA Airport/src/Airport/BagCheck.scala
+++ TSA Airport/src/Airport/BagCheck.scala
@@ -12,20 +12,26 @@ class BagCheck (percentage: Int, lines: Int) extends Actor{
 	def act () {
 	  loop {
 	    receive{
-	      case "check" =>
+	      case check: String =>
 	        openLines = openLines - 1 
-	        Thread.sleep(100) // some sort of time passes while checking
+	        // if stop is received exit the loop
+	        if (check == "stop")
+	          exit
+	          
+	        // Check to see if there is an open line
 	        if (openLines > 0){
+	        	Thread.sleep(100) // some sort of time passes while checking
 	        	if (randNumber.nextInt < percentage)
-	        		sender ! "passed"
+	        		sender ! check + "passed"
 	        	else
-	        		sender ! "failed"
+	        		sender ! check + "failed"
 	        }else{
 	          sender ! "busy" // I will send busy to Line if there are no lines available
 	        }
-	        openLines = openLines + 1 // reopen that particular line
-	      case "stop" => // If I receive stop, this will end kill the actor
-	        exit
+	        openLines = openLines + 1 // reopen a line
+	        
+	        
+	      // this is the old stuff 
 //	      case "Tester" => 
 //	      	if (randNumber.nextInt() < percentage)
 //	      	  System.out.println("I passed");
