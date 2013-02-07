a03da3df6ffa8e32342e58f38ea35c1e131f4ee5
Chris <celdridge91190@gmail.com>
2013-02-06 21:54:42 -0500

    This is pretty much what bagcheck will look like

--- TSA Airport/src/Airport/BagCheck.scala
+++ TSA Airport/src/Airport/BagCheck.scala
@@ -3,21 +3,37 @@ import scala.actors.Actor
 import scala.util.Random
 
 
-
-class BagCheck () extends Actor{
-  var randNumber = new Random(100);
-  var percentage = 50;
+/**
+ * Takes in percentage of pass/fail rate and number of lines this actor should have
+ */
+class BagCheck (percentage: Int, lines: Int) extends Actor{
+  var randNumber = new Random(100); // number between 1 and 100 to determine pass/fail
+  var openLines = lines // how many lines are available for this one 
 	def act () {
 	  loop {
 	    receive{
-	      case "Tester" => 
-	      	if (randNumber.nextInt() < percentage)
-	      	  System.out.println("I passed");
-	      	else 
-	      	  System.out.println("I failed");
-	      case "Stop" => 
-	        sender ! "Stop"
-	        exit()
+	      case "check" =>
+	        openLines = openLines - 1 
+	        Thread.sleep(100) // some sort of time passes while checking
+	        if (openLines > 0){
+	        	if (randNumber.nextInt < percentage)
+	        		sender ! "passed"
+	        	else
+	        		sender ! "failed"
+	        }else{
+	          sender ! "busy" // I will send busy to Line if there are no lines available
+	        }
+	        openLines = openLines + 1 // reopen that particular line
+	      case "stop" => // If I receive stop, this will end kill the actor
+	        exit
+//	      case "Tester" => 
+//	      	if (randNumber.nextInt() < percentage)
+//	      	  System.out.println("I passed");
+//	      	else 
+//	      	  System.out.println("I failed");
+//	      case "Stop" => 
+//	        sender ! "Stop"
+//	        exit()
 	    }
 	    
 	    	
