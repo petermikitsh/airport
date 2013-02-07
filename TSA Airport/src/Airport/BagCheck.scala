3e7f53ad9410793472ec109cb6498688e1bd959f
Chris <celdridge91190@gmail.com>
2013-02-06 21:27:31 -0500

    Added some very simple stuff to BagCheck just to get a feel for how Scala works

--- TSA Airport/src/Airport/BagCheck.scala
+++ TSA Airport/src/Airport/BagCheck.scala
@@ -1,8 +1,27 @@
 package Airport
 import scala.actors.Actor
+import scala.util.Random
 
-class BagCheck extends Actor{
+
+
+class BagCheck () extends Actor{
+  var randNumber = new Random(100);
+  var percentage = 50;
 	def act () {
-	  
+	  loop {
+	    receive{
+	      case "Tester" => 
+	      	if (randNumber.nextInt() < percentage)
+	      	  System.out.println("I passed");
+	      	else 
+	      	  System.out.println("I failed");
+	      case "Stop" => 
+	        sender ! "Stop"
+	        exit()
+	    }
+	    
+	    	
+	  }
+		  
 	}
 }
