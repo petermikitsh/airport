3bc342e7f1dede08b453b1903eda9be80db6c1e9
Chris <celdridge91190@gmail.com>
2013-02-08 11:41:26 -0500

    Changed BagCheck to use case classes (you can send parameters with these)

--- TSA Airport/src/Airport/BagCheck.scala
+++ TSA Airport/src/Airport/BagCheck.scala
@@ -2,17 +2,21 @@ package Airport
 import scala.actors.Actor
 import scala.util.Random
 
+case class check (result: String, name: String)
+
 
 /**
  * Takes in percentage of pass/fail rate and number of lines this actor should have
  */
 class BagCheck (percentage: Int, lines: Int) extends Actor{
+  var result = ""; 
+  var name = "";
   var randNumber = new Random(100); // number between 1 and 100 to determine pass/fail
   var openLines = lines // how many lines are available for this one 
 	def act () {
 	  loop {
 	    receive{
-	      case check: String =>
+	      case s: String =>
 	        openLines = openLines - 1 
 	        // if stop is received exit the loop
 	        if (check == "stop")
@@ -22,9 +26,11 @@ class BagCheck (percentage: Int, lines: Int) extends Actor{
 	        if (openLines > 0){
 	        	Thread.sleep(100) // some sort of time passes while checking
 	        	if (randNumber.nextInt < percentage)
-	        		sender ! check + "passed"
-	        	else
-	        		sender ! check + "failed"
+	        		result = "passed"
+	        	else{
+	        		result = "failed"
+	        	}			
+	        	sender ! check(result, name);
 	        }else{
 	          sender ! "busy" // I will send busy to Line if there are no lines available
 	        }
