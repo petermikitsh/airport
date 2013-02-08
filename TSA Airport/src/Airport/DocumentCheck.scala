d4cb6a091ab76ecab4bf403f6e17574b265eda1e
Cory <cms5822@rit.edu>
2013-02-07 21:47:04 -0500

    Basic outlone
    I have a basic setup for the DocumentCheck, need to do a bit more on
    it.

--- TSA Airport/src/Airport/DocumentCheck.scala
+++ TSA Airport/src/Airport/DocumentCheck.scala
@@ -1,8 +1,64 @@
 package Airport
 import scala.actors.Actor
+import scala.util.Random
 
-class DocumentCheck extends Actor{
-	def act() {
-	  
-	}
+class DocumentCheck (queues: List) extends Actor{
+  
+  //20% probability that the passenger will fail
+  var failureRate = 20;
+  // Which queue to hand the passenger off to next
+  var queueNumber = 0;
+  // Random generator for determining if the passenger fails
+  var randNumber;
+  // List for holding all the queues
+  val queues = queues; 
+  
+  def act() {
+    loop{
+      receieve{
+        case check: String =>
+          if(passed()){
+            logSentToQueue("Name");
+             this.queues.get(this.queueNumber) ! "passenger"
+             this.queueNumber++;
+             if(this.queueNumber == this.queues.size())
+               this.queueNumber = 0;
+          }
+          else{
+            logTurnedAway("Passenger");
+          }
+        case close: String =>
+          //loop through the actor queue and send close messages
+          logClosing();
+          
+          
+      }
+    }
+  }
+  
+  def passed(){
+    var passed = true;
+    randNumber = new Random(100);
+    if(randNumber < failureRate)
+      passed = false;
+    
+    return passed;
+  }
+  
+  def logArrival (passenger: String){
+    println("Document Check: Passenger name arrives"); // Somehow get passenger's name
+  }
+  
+  def logTurnedAway(passenger: String){
+    println("Document Check: Passenger name turned away"); // Get passenger name
+  }
+  
+  def logSentToQueue(passenger: String){
+    println("Document Check: Passenger name sent to line " + queueNumber);
+  }
+  
+  def logClosing(){
+    println("Document Check: Close sent");
+    println("Document Check: Closed);
+  }
 }
